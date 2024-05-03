7fd32576c4a511b5b3c981139ced32a837d01da5
Kinuganjhu <kinuganjhu02@gmail.com>
2024-05-03 16:00:50 +0530

    added github pages

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,52 +1,62 @@
-import {db} from '../api/Firebase'
-import {useState,useEffect} from 'react'
-import {ref,push,onValue} from 'firebase/database'
-const Home = ()=>{
-  const[message,setMessage] = useState(' ')
-  const[userMessage,setUserMessage] = useState([]);
-  
-  useEffect(()=>{
-    
-   const fetchMessage =()=>{
-     const messageRef = ref(db,'users')
-     onValue(messageRef,(snapshot)=>{
-       const userMessages= snapshot.val();
-       if(userMessages){
-      const messageList = Object.keys(userMessages).map((userId)=>({
-        id: userId,
-        message:userMessages[userId].message,
-      }))
-      setUserMessage(messageList)
-       }
-     })
-   }
-   fetchMessage();
-   return ()=>{
-     //
-   }
-     
-    
-  },[])
-  const handleChangeText =(e)=>{
-    setMessage(e.target.value);
+import { db } from '../api/Firebase'
+import { useState, useEffect } from 'react'
+import { ref, push, onValue, remove } from 'firebase/database'
+
+const Home = () => {
+  const [message, setMessage] = useState('')
+  const [userMessage, setUserMessage] = useState([])
+
+  useEffect(() => {
+    const fetchMessage = () => {
+      const messageRef = ref(db, 'users')
+      onValue(messageRef, (snapshot) => {
+        const userMessages = snapshot.val()
+        if (userMessages) {
+          const messageList = Object.keys(userMessages).map((userId) => ({
+            id: userId,
+            message: userMessages[userId].message,
+          }))
+          setUserMessage(messageList)
+        }
+      })
+    }
+    fetchMessage()
+    return () => {
+      //
+    }
+  }, [])
+
+  const handleChangeText = (e) => {
+    setMessage(e.target.value)
   }
-  const handleSendMessage =()=>{
-    const messageRef = ref(db,'users')
-    push(messageRef,({
+
+  const handleSendMessage = () => {
+    const messageRef = ref(db, 'users')
+    push(messageRef, {
       message: message
-    }))
-    setMessage(' ')
+    })
+    setMessage('')
   }
-  return(
+
+  const handleDeleteMessage = (id) => {
+    const messageRef = ref(db, `users/${id}`)
+    remove(messageRef)
+  }
+
+  return (
     <>
-    {userMessage.map((item)=>(
-      <h6 key ={item.id}>{item.message}</h6>
+      {userMessage.map((item) => (
+        <h6 key={item.id}>
+          {item.message}
+          <button onClick={() => handleDeleteMessage(item.id)}>delete</button>
+        </h6>
       ))}
-      <div className ='bottom'>
-    <input type='text' value ={message} placeholder ='enter message' onChange={handleChangeText}/>
-    <button onClick={handleSendMessage}>send message</button>
-    </div>
+      <div className='bottom'>
+        <input type='text' value={message} placeholder='enter message' onChange={handleChangeText} />
+        <button onClick={handleSendMessage}>send message</button>
+      </div>
     </>
-    )
+  )
 }
+
 export default Home;
