520ca9fa2c6d25b56ca4c4616e3b4dac0f9ba7d4
Kinuganjhu <kinuganjhu02@gmail.com>
2024-05-03 14:59:40 +0530

    messaging option is completed i think

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,56 +1,52 @@
-import { db } from '../api/Firebase';
-import UploadFile from './UploadFile'
-import { useState, useEffect } from 'react';
-import { ref, push, onValue } from 'firebase/database';
-
-
-export default function Home() {
-  const [message, setMessage] = useState('');
-  const [userMessage, setUserMessage] = useState([]);
-
-  useEffect(() => {
-    const messageRef = ref(db, 'users');
-    const fetchMessage = () => {
-      onValue(messageRef, (snapshot) => {
-        const userMessages = snapshot.val();
-        if (userMessages) {
-          const messageList = Object.keys(userMessages).map((userId) => ({
-            id: userId,
-            message: userMessages[userId].message,
-          }));
-          setUserMessage(messageList);
-        }
-      });
-    };
-    fetchMessage();
-    return () => {
-      //cleanup function
-    };
-  }, []);
-
-  const handleSendMessage = () => {
-    const messageRef = ref(db, 'users');
-    push(messageRef, {
+import {db} from '../api/Firebase'
+import {useState,useEffect} from 'react'
+import {ref,push,onValue} from 'firebase/database'
+const Home = ()=>{
+  const[message,setMessage] = useState(' ')
+  const[userMessage,setUserMessage] = useState([]);
+  
+  useEffect(()=>{
+    
+   const fetchMessage =()=>{
+     const messageRef = ref(db,'users')
+     onValue(messageRef,(snapshot)=>{
+       const userMessages= snapshot.val();
+       if(userMessages){
+      const messageList = Object.keys(userMessages).map((userId)=>({
+        id: userId,
+        message:userMessages[userId].message,
+      }))
+      setUserMessage(messageList)
+       }
+     })
+   }
+   fetchMessage();
+   return ()=>{
+     //
+   }
+     
+    
+  },[])
+  const handleChangeText =(e)=>{
+    setMessage(e.target.value);
+  }
+  const handleSendMessage =()=>{
+    const messageRef = ref(db,'users')
+    push(messageRef,({
       message: message
-    });
-    setMessage('');
-  };
-
-  return (
-    <div>
-      <h4>Realtime Messaging</h4>
-      <div>
-        {userMessage.map((item) => (
-          <div key={item.id}>
-            {item.message}
-          </div>
-        ))}
-      </div>
-      <div className="input-container">
-        <input type='text' placeholder='enter message' value={message} onChange={(e) => setMessage(e.target.value)} />
-        <UploadFile/>
-        <button onClick={handleSendMessage}>send message</button>
-      </div>
+    }))
+    setMessage(' ')
+  }
+  return(
+    <>
+    {userMessage.map((item)=>(
+      <h6 key ={item.id}>{item.message}</h6>
+      ))}
+      <div className ='bottom'>
+    <input type='text' value ={message} placeholder ='enter message' onChange={handleChangeText}/>
+    <button onClick={handleSendMessage}>send message</button>
     </div>
-  );
+    </>
+    )
 }
+export default Home;
